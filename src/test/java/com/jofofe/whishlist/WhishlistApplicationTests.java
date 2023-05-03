d5d2285688684de5cefcfc3bf969375bfcd5138f
João Ferreira <joao.ferreira@luizalabs.com>
2023-05-03 15:48:48 -0300

    Initial configurations.

---
+++ src/test/java/com/jofofe/whishlist/WhishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jofofe.whishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
